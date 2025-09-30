e7dc2f9efca61588b1aeedee4754a8b932af51c8
Connor Counceller <111522793+connor-counceller@users.noreply.github.com>
2025-09-30 11:32:08 +0000

    chore: set up tanstack

---
+++ src/Blog.jsx
@@ -0,0 +1,29 @@
+import { PostList } from './components/PostList.jsx'
+import { CreatePost } from './components/CreatePost.jsx'
+import { PostFilter } from './components/PostFilter.jsx'
+import { PostSorting } from './components/PostSorting.jsx'
+import { useQuery } from '@tanstack/react-query'
+import { getPosts } from './api/posts.js'
+
+const postsQuery = useQuery({
+  queryKey: ['posts'],
+  queryFn: () => getPosts(),
+})
+
+const posts = postsQuery.data ?? []
+
+export function Blog() {
+  return (
+    <div style={{ padding: 8 }}>
+      <CreatePost />
+      <br />
+      <hr />
+      Filter by:
+      <PostFilter field='author' />
+      <br />
+      <PostSorting fields={['createdAt', 'updatedAt']} />
+      <hr />
+      <PostList posts={posts} />
+    </div>
+  )
+}
