aca3a72ab46e2769526c9bbc864312eac7a31909
Connor Counceller <111522793+connor-counceller@users.noreply.github.com>
2025-10-01 14:59:23 +0000

    chore: remove old blog and post files

--- src/api/posts.js
+++
@@ -1,7 +0,0 @@
-export const getPosts = async (queryParams) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/posts?` +
-      new URLSearchParams(queryParams),
-  )
-  return await res.json()
-}
